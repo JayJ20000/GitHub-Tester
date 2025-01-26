78c4ad3a9719c1b678443eb87070035eaad02cd6
Jalen Jones <jalcjon1@ut.utm.edu>
2025-01-25 23:38:45 -0600

    second main commit

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
-
 #include <iostream>
 
 using namespace std;
 
+  int sum(int n){
+      int total = 0;
+    for(int i = 1; i <= n; i++){
+        total+=i;
+    }
+    return total;
+  }
+
+
 int main() {
+  int n;
   
+  cout << "Enter a number: ";
+  cin >> n;
   
-  return 0;
-}
+  cout << "The sum of the numbers up to n are: " << sum(n) << "\n";
+ }
